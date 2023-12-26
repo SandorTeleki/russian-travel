d17031e1c4b2823383c35caf9f1df2c81231667f
Sandor Teleki <sandorteleki1@gmail.com>
2023-12-26 09:41:49 +0100

    feat: finished translation, added copyright year, linting+stylistic fixes

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const russianButton = document.getElementById('russianButton');
 const russianSection = document.getElementById('russian-section');
 const englishButton = document.getElementById('englishButton');
 const englishSection = document.getElementById('english-section');
+const year = document.getElementById("year");
+
+const currentYear = new Date().getFullYear();
+year.innerText = `©${currentYear}`
 
 russianButton.addEventListener('click', () => {
     russianButton.classList.add("header__lang-link_active");
     englishButton.classList.remove("header__lang-link_active");
     russianSection.style.display = 'block';
     englishSection.style.display = 'none';
+    document.title = "Путешествие по России";
+    document.documentElement.lang = 'ru';
 })
 
 englishButton.addEventListener('click', () => {
@@ -15,4 +21,6 @@ englishButton.addEventListener('click', () => {
     englishButton.classList.add("header__lang-link_active");
     russianSection.style.display = 'none';
     englishSection.style.display = 'block';
+    document.title = "Travel through Russia";
+    document.documentElement.lang = 'en';
 })
