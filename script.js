d0539c9f67162529e67056d923db818cd4fb09a2
Sandor Teleki <sandorteleki1@gmail.com>
2023-12-27 07:45:32 +0100

    feat: added browser language check to swap default language

--- script.js
+++ script.js
@@ -9,20 +9,28 @@ const currentYear = new Date().getFullYear();
 yearRU.innerText = `©${currentYear} `;
 yearEN.innerText = `©${currentYear} `;
 
-russianButton.addEventListener('click', () => {
-    russianButton.classList.add("header__lang-link_active");
-    englishButton.classList.remove("header__lang-link_active");
-    russianSection.style.display = 'block';
-    englishSection.style.display = 'none';
-    document.title = "Путешествие по России";
-    document.documentElement.lang = 'ru';
-})
-
-englishButton.addEventListener('click', () => {
+function toEnglish(){
     russianButton.classList.remove("header__lang-link_active");
     englishButton.classList.add("header__lang-link_active");
     russianSection.style.display = 'none';
     englishSection.style.display = 'block';
     document.title = "Travel through Russia";
     document.documentElement.lang = 'en';
-})
+}
+
+function toRussian(){
+    russianButton.classList.add("header__lang-link_active");
+    englishButton.classList.remove("header__lang-link_active");
+    russianSection.style.display = 'block';
+    englishSection.style.display = 'none';
+    document.title = "Путешествие по России";
+    document.documentElement.lang = 'ru';
+}
+
+//Event Listeners
+russianButton.addEventListener('click', toRussian);
+englishButton.addEventListener('click', toEnglish);
+
+if(navigator.language === 'en-US'){
+    toEnglish();
+}
